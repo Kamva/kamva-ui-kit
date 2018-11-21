06a2c46b65ceb6450cdb8038a4a612abf502904e
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2018-11-21 13:08:00 +0330

    update docs

--- src/docs.tsx
+++ src/docs.tsx
@@ -1,16 +1,12 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { Button } from "./index";
 import "./style/style.css";
-import { colorPallet } from "./utils/style";
+import { PrimaryButtons } from "./components/docs/primaryButtons";
 
 const Layout = () => {
   return (
     <div className="container">
-      <div className="buttons">
-        <h2>Buttons</h2>
-        <Button color={colorPallet.third} disable={true}>شروع با کاموا</Button>
-      </div>
+      <PrimaryButtons />
     </div>
   );
 };
