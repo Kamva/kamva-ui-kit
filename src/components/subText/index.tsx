01faa93591c4826034cce896e241c9d731c0bbc1
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-02-21 19:20:25 +0330

    change subText element to paragraph

--- src/components/subText/index.tsx
+++ src/components/subText/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import styled from "styled-components";
 import { colorPallet, IHeadingProps } from "../../types";
 
-export const SubText = styled("h6")<IHeadingProps>`
+export const SubText = styled("p")<IHeadingProps>`
   color: ${props => (props.color ? props.color : colorPallet.subText)};
   font-size: ${props => (props.fontSize ? props.fontSize : "1rem")};
   font-weight: ${props => (props.fontWeight ? props.fontWeight : "300")};
